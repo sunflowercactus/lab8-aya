8829c90cdb5f2bf7ef4241e56012cc14a0d8a686
soboleva_dasha <115922130+sunflowercactus@users.noreply.github.com>
2024-05-22 16:53:35 +0300

    Add files via upload

---
+++ MyShare-lab08.cpp
@@ -0,0 +1,59 @@
+﻿#include <iostream>
+
+using namespace std;
+
+template<typename T>
+class MyShared {
+private:
+    T* ptr;
+    int* count;
+
+public:
+    MyShared(T* ptr) : ptr(ptr), count(new int(1)) {}
+
+    MyShared(const MyShared& other) : ptr(other.ptr), count(other.count) {
+        (*count)++;
+    }
+
+    ~MyShared() {
+        (*count)--;
+        if ((*count) == 0) {
+            delete ptr;
+            delete count;
+            cout << "Memory freed!" << endl;
+        }
+    }
+
+    T& operator*() {
+        return *ptr;
+    }
+
+    T* operator->() {
+        return ptr;
+    }
+};
+
+template<typename T, typename... Args>
+MyShared<T> Make_MyShared(Args&&... args) {
+    return MyShared<T>(new T(std::forward<Args>(args)...));
+}
+
+class MyClass {
+public:
+    void hello() {
+        cout << "Hello from MyClass!" << endl;
+    }
+};
+
+int main() {
+    auto ptr1 = Make_MyShared<MyClass>();
+    auto ptr2 = ptr1;
+
+    cout << "Using ptr1: ";
+    ptr1->hello();
+
+    cout << "Using ptr2: ";
+    ptr2->hello();
+
+    return 0;
+}
---
+++ laba8-aya.cpp
@@ -0,0 +1,61 @@
+﻿#include <iostream>
+using namespace std;
+
+
+template<typename T>
+class SmartPtr {
+private:
+    T* ptr;
+
+public:
+    SmartPtr(T* p = nullptr) : ptr(p) {}
+
+    ~SmartPtr() {
+        delete ptr;
+    }
+
+    SmartPtr(const SmartPtr& other) = delete;
+
+    SmartPtr& operator=(const SmartPtr& other) = delete;
+
+    SmartPtr(SmartPtr&& other) {
+        ptr = other.ptr;
+        other.ptr = nullptr;
+    }
+
+    SmartPtr& operator=(SmartPtr&& other) {
+        if (this != &other) {
+            delete ptr;
+            ptr = other.ptr;
+            other.ptr = nullptr;
+        }
+        return *this;
+    }
+
+    T* get() const {
+        return ptr;
+    }
+
+    T& operator*() const {
+        return *ptr;
+    }
+
+    T* operator->() const {
+        return ptr;
+    }
+};
+
+int main() {
+    SmartPtr<int> ptr1(new int(42));
+    SmartPtr<int> ptr2(move(ptr1));
+
+    if (ptr1.get() == nullptr) {
+        cout << "ptr1 is now empty\n";
+    }
+
+    *ptr2 = 100;
+    
+    cout << "*ptr2: " << *ptr2 << endl;
+
+    return 0;
+}
